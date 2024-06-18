6fcbea182c5d5b23bb579705a414eeef91efc8e6
Gihan <gihanvimukthi19@gmail.com>
2024-06-19 01:29:05 +0530

    second commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyparser = require('body-parser');
 const { Socket } = require('dgram');
+const mongoose = require('mongoose')
 
 const app = express();
 const http = require('http').Server(app)
@@ -10,24 +11,45 @@ app.use(express.static(__dirname))
 app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({extended: false}))
 
-const messages = [{
-    name: "gihan",
-    message:"hii"
-}]
+const dbUrl = "mongodb+srv://gihanvimukthi19:VQjgCTIHmSILzj3M@cluster0.byx59wk.mongodb.net/learning-node"
+
 
 app.get('/messages', (req , res) =>{
-    res.send(messages)
+    Message.find({}).then((err, messages) => {
+        res.send(messages)
+    })
+
+    
 })
 
-app.post('/messages', (req , res) =>{
-    console.log(req.body)
-    io.emit('message' , req.body)
-    messages.push(req.body)
-    res.sendStatus(200)
+const Message = mongoose.model('Message' , {
+    name: String,
+    message: String
 })
+
+app.post('/messages', (req, res) => {
+    const message = new Message(req.body);
+
+    message.save()
+        .then(() => {
+            io.emit('message', req.body);
+            res.sendStatus(200);
+        })
+        .catch((err) => {
+            console.error('Error saving message:', err);
+            if (!res.headersSent) { // Ensure headers are not already sent
+                res.sendStatus(500);
+            }
+        });
+});
+
 
 io.on('connect' , (Socket) =>{
     console.log("user connected")
+})
+
+mongoose.connect(dbUrl).then(() =>{
+    console.log("mongodb connected")
 })
 
 const server = http.listen(3000 , () =>{
